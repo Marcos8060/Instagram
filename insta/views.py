c61f63d1d7da9f6e755aa728c7dbbdcd2e6000b4
Marcos8060 <marcosgav80@gmail.com>
2022-04-07 20:27:24 +0300

    added a signup page

--- insta/views.py
+++ insta/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+
+def register(request):
+    return render(request,'register.html')
