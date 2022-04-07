9d5a978b09d395fa99eb658fa8b31e12f6ac8d03
Marcos8060 <marcosgav80@gmail.com>
2022-04-07 22:25:39 +0300

    created the register logic

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -6,6 +8,22 @@ def home(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password'] 
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request,'Username already taken!')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,'Email already exists!')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username=username,email=email,password=password)
+            user.save()
+            print('user created')
+            return redirect('login')
     return render(request,'register.html')
 
 
