cac6fb61e2d2ff0e0e935f7ab974f733acd5144d
Marcos8060 <marcosgav80@gmail.com>
2022-04-08 00:03:54 +0300

    made model migrations

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile_pictures',null=False)
+    title = models.CharField(max_length=30)
+    caption = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    liked = models.ManyToManyField(User,related_name='liked',default=None,blank=True)
 
+    def __str__(self):
+        return self.title
--- instagram/settings.py
+++ instagram/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'marcos',
+        'PASSWORD':'getaways',
     }
 }
 
