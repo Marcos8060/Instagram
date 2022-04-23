2c171e06aa0c6824d16a4c27dde02f2868e6c6a2
Marcos8060 <marcosgav80@gmail.com>
2022-04-23 19:11:27 +0300

    made changes in the url routes

--- insta/urls.py
+++ insta/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('register/',views.register,name='register'),
+    path('/home',views.home,name='home'),
+    path('',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
 ]
